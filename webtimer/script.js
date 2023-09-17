572f8b9ef21ce10bbcc0e9bd894c6b2e75c261cd
ckdev88 <ckdev88@outlook.com>
2023-09-17 18:22:44 -0300

    see if this works

--- webtimer/script.js
+++ webtimer/script.js
@@ -17,11 +17,13 @@ task_new_quick
 task_container
 */
 
+const getTasks = () => { return JSON.parse(localStorage.getItem('timerTasks')); }
+const updateTasks = (arr) => { localStorage.setItem('timerTasks', JSON.stringify(arr)); }
+
 function newTaskSubmit() {
 	task_new_form.addEventListener("submit", (e) => {
 		e.preventDefault();
 		var data = new FormData(document.getElementById("task_new_form"));
-		// console.log(data.get('task_start_now'));
 		addTask(
 			data.get("task_name"),
 			data.get("task_description"),
@@ -31,85 +33,57 @@ function newTaskSubmit() {
 		document.getElementById("new_task_name").value = "";
 		document.getElementById("new_task_description").value = "";
 		document.getElementById("new_task_interval").value = "";
-		// data.delete('task_name');
 	});
 }
 newTaskSubmit();
 
+
 function addTask(name, description, interval) {
 	if (name) {
-		// get item timerTasks
-		let timerArr = [];
-		timerArr = JSON.parse(localStorage.getItem("timerTasks"));
-		// console.log(timerArr);
+		let timerArr = getTasks();
 
-		// push into item timerTasks
 		timerArr.push({
 			name: name,
 			description: description,
 			interval: interval,
+			timepast: 0,
 			task: "task-" + name.replaceAll(' ', ''),
 		});
 
-		// set item timerTasks
-		localStorage.setItem("timerTasks", JSON.stringify(timerArr));
-		let timerArr2 = JSON.parse(localStorage.getItem("timerTasks"));
+		updateTasks(timerArr);
+		let timerArr2 = getTasks();
 		renderTasks(timerArr2);
 	}
 }
 
-// function detectQuickTask() { // temparily not using
-// 	if (document.getElementById("task-Stretch")) {
-// 		task_new_quick.className = "dnone";
-// 	} else task_new_quick.className = "dblock";
-// }
-
-let startArr = [];
-localStorage.setItem("timerTasks", JSON.stringify(startArr));
-
-let getTimerTasksArr = JSON.parse(localStorage.getItem("timerTasks"));
 
 // ADD TASKS
-let tasks = [];
-// Add task
-// properties:
-//	id<int><auto-increment><hidden>
-//	name<string><required>
-//	description<string><optional>
-//	start<string><HH:MM>/Now<required>, default=Now
-//	interval (minutes)<int>, default=false
-
-localStorage.setItem("taken", JSON.stringify(tasks));
 
 // use quick add function so have a base setup, using all basic fields and characteristics
 task_new_quick.addEventListener("click", () => {
 	addQuickTask();
-	// detectQuickTask(); // tijdelijk buiten gebruik, zie ook andere soortgelijke regels mbt detect..
-
-	// TODO: check on name if not exists to prevent duplicates
-	// getTimerTasksArr.push({
-	// 	name: 'stretch',
-	// 	description: 'Even lopen, strekken, eten, niets superactiefs',
-	// 	interval: 35,
-	// });
-	// localStorage.setItem('timerTasks', JSON.stringify({
-	// 	getTimerTasksArr
-	// }));
-	// console.log(getTimerTasksArr);
-	// console.log(getTimerTasksArr.length);
-	//
-
-	// TODO: verwijs naar functie die de update uitvoert, ipv bovenstaand
 });
 
-//hide quick add button when there a quickly addes task already exists
+let getTimerTasksArr = getTasks();
 
-function renderTasks(getTimerTasksArr) {
-	task_container.innerHTML = "";
-	for (let i = 0; i < getTimerTasksArr.length; i++) {
-		task_container.appendChild(renderTask(getTimerTasksArr[i], i));
+// Show New task-form if tasks array is empty
+const checkTasksEmpty = () => {
+	if (!getTasks()) updateTasks([]); // if array doesnt exist at all, create it
+	if (getTasks().length === 0) {
+		return true;
+	} else {
+		return false;
 	}
 }
+if (checkTasksEmpty() === true) task_new_form.className = "dblock";
+
+function renderTasks(arr) {
+	task_container.innerHTML = "";
+	for (let i = 0; i < arr.length; i++) {
+		task_container.appendChild(renderTask(arr[i], i));
+	}
+}
+renderTasks(getTasks());
 
 function renderTask(i, key) {
 	let el = document.createElement("div");
@@ -118,8 +92,7 @@ function renderTask(i, key) {
 	el.appendChild(renderTaskElement("h3", "task-name", i.name));
 	el.appendChild(renderTaskElement("div", "task-description", i.description));
 	el.appendChild(renderTaskElement("div", "task-countdown-total", i.interval));
-	el.appendChild(renderTaskElement('div', 'task-countdown-current', countdownTimer(i.interval, 'countdown-task-' + key), 'countdown-' + el.id));
-	// el.appendChild(renderTaskElement('div', 'task-countdown-current2', countdownTimer2(i.interval, 'countdown-task-' + key), 'countdown2-' + el.id));
+	el.appendChild(renderTaskElement('div', 'task-countdown-current', countdownTimer(i.interval, key, 'countdown-task-' + key), 'countdown-' + el.id));
 	el.appendChild(removeTaskLink(key));
 	return el;
 }
@@ -128,44 +101,41 @@ function renderTaskElement(node = "div", className, content, id = undefined) {
 	let taskEl = document.createElement(node);
 	taskEl.className = className;
 	taskEl.innerHTML = content;
-	id !== 'undefined' ? taskEl.id = id : '';
+	id !== undefined ? taskEl.id = id : '';
 	return taskEl;
 }
 
 function addQuickTask() {
 	let timerArr = [];
-	timerArr = JSON.parse(localStorage.getItem("timerTasks"));
+	timerArr = getTasks();
 
 	timerArr.push({
 		name: "Stretch",
 		description: "Quick timer",
 		interval: 35,
-		task: "task-Stretch",
+		timepast: 0,
+		// task: "task-Stretch",
 	});
 
 	// set item timerTasks
-	localStorage.setItem("timerTasks", JSON.stringify(timerArr));
-	let timerArr2 = JSON.parse(localStorage.getItem("timerTasks"));
+	updateTasks(timerArr);
+	let timerArr2 = getTasks();
 	renderTasks(timerArr2);
 }
 
 function removeTaskLink(key) {
-	console.log('removeTaskLink:', key);
 	let el = document.createElement("button");
 	el.innerHTML = "remove task";
 	el.className = "text";
-	el.className += " teeee";
 	el.id = 'del-' + key;
 	el.addEventListener("click", () => {
 		removeTask(key);
-		// detectQuickTask(); // tijdelijk buiten gebruik
 	});
 	return el;
 }
 
 function removeTask(key) {
-	console.log('removeTask:', key);
-	let arr = JSON.parse(localStorage.getItem("timerTasks"));
+	let arr = getTasks();
 
 	let newarr = [];
 	for (let i = 0; i < arr.length; i++) {
@@ -174,80 +144,50 @@ function removeTask(key) {
 			name: arr[i].name,
 			description: arr[i].description,
 			interval: arr[i].interval,
-			task: "task-" + arr[i].name.replaceAll(' ', ''),
+			timepast: arr[i].timepast,
+			// task: "task-" + arr[i].name.replaceAll(' ', ''),
 		});
 	}
-	localStorage.setItem("timerTasks", JSON.stringify(newarr));
+	updateTasks(newarr);
 	renderTasks(newarr);
 }
-
-function updateTasks(from, to) {
-	console.log("update tasks...");
-}
-
-updateTasks(JSON.parse(localStorage.getItem("timerTasks")));
-
-// - Countdown timer
-// - Button: if interval==false: DONE, if interval==true: RESET
-
-
-function countdownTimer(limit, id, currentCount = 0) {
-	let trigger = setInterval((sec = limit) => {
-		currentCount = timer(currentCount);
-		if (currentCount == sec) { console.log('stop'); stopIntervalObj(trigger); }
+function countdownTimer(limit, key, id) {
+	const lb = setInterval((max = limit, id2 = id) => {
+		if (document.getElementById(id)) {
+			let arr = getTasks();
+			if (arr[key].timepast > max) stopit();
+			document.getElementById(id2).innerHTML = arr[key].timepast;
+		}
 	}, 1000);
-
-	function timer(next) {
-		next += 1;
-		document.getElementById(id).innerHTML = next;
-		return next;
-	}
-}
-
-function stopIntervalObj(obj) {
-	clearInterval(obj);
-}
-
-// var t = document.createElement('div');
-// t.id = 'testnum';
-// document.body.appendChild(t);
-
-function countdownTimer2(limit, id) {
-	console.log('id:', id);
-	console.log('limit:', limit);
-	const lb =
-		setInterval((max = limit, id) => {
-			if (document.getElementById(id) === 'undefined') { document.getElementById(id).innerHTML = 0; }
-			let nunum = document.getElementById(id).innerHTML;
-			addone(Number(nunum));
-			if (Number(nunum) === (Number(max) - 1)) stopit();
-		}, 1000);
-	function addone(oldNumber) {
-		let newNumber = oldNumber + 1;
-		document.getElementById(id).innerHTML = newNumber;
-		return
-	}
 	function stopit() {
-		console.log('stopit()');
 		clearInterval(lb);
 	}
 }
-// countdownTimer2();
 
+function countdownAll() {
+	setInterval(() => {
+		let arr = getTasks();
+		for (let i = 0; i < arr.length; i++) {
+			if (arr[i].timepast < arr[i].interval) arr[i].timepast++;
+		}
+		updateTasks(arr);
+	}, 1000)
+
+}
+countdownAll();
 
 
 // TODO: organize whatever is below this line
 // -------------------------------------------------------------------------------------
 
-// Show New task-form if tasks array is empty
-if (getTimerTasksArr.length === 0) task_new_form.className = "dblock";
 
 // let button #new_task_btn toggle the form #new_task_form
-// TODO: temporarily commented 
 task_new_btn.addEventListener("click", () => {
-
 	!task_new_form.checkVisibility()
 		? (task_new_form.className = "dblock")
 		: (task_new_form.className = "dnone");
 });
+
+// - Countdown timer
+// - Button: if interval==false: DONE, if interval==true: RESET
 
