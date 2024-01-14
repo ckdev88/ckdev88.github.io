01de3d030a9399670ce7ad24617f673f37223860
ckdev88 <ckdev88@outlook.com>
2024-01-14 15:31:12 -0300

    remove more

--- src/components/VisitorsSupabase.ts
+++
@@ -1,3 +0,0 @@
-// TODO: use or remove
-//const currentUrl = Astro.url.href;
-const currentUrl = 'tsxtest.com';
--- src/vs.js
+++
@@ -1,8 +0,0 @@
-// TODO: need to be either cleaned up or used, also see pages/nl/index.astro
-import supabase from "./supabase";
-const currentUrl = "NLLLLL";
-await supabase
-  .rpc("count_visits", {
-    url: currentUrl,
-  })
-  .single();
