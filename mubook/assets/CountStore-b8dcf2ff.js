fc09be3944447810e810f8a752ad5a411377a06a
ckdev88 <ckdev88@outlook.com>
2024-01-18 07:05:29 -0300

    mubooks-update

---
+++ mubook/assets/CountStore-b8dcf2ff.js
@@ -0,0 +1,1 @@
+import{I as e}from"./index-41ca3bb9.js";const o=e("CountStore",{state:()=>({count:0,incrementAmount:34}),actions:{increment(){this.count+=this.incrementAmount}},getters:{doubleCount:t=>t.count*2}});export{o as u};
---
+++ mubook/assets/SupabaseTest-ffeca49e.js
@@ -0,0 +1,1 @@
+import{r as o,B as u,o as e,c as a,F as c,l,G as i,t as p}from"./index-41ca3bb9.js";const d={__name:"SupabaseTest",setup(m){const t=o([]);async function r(){const{data:s}=await i.from("countries").select();t.value=s}return u(()=>{r()}),(s,_)=>(e(),a("ul",null,[(e(!0),a(c,null,l(t.value,n=>(e(),a("li",{key:n.id},p(n.name),1))),128))]))}};export{d as default};
