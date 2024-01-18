7cbd97f2f778146fd7f51840b53e0e9409f7f67b
ckdev88 <ckdev88@outlook.com>
2024-01-18 18:32:52 -0300

    mubooks-update

---
+++ mubook/assets/SavedBooksPage-b9a65f0d.js
@@ -0,0 +1,1 @@
+import{B as e}from"./MuBooksOverview-609f4576.js";import{o,c as a,e as s,F as t,a as r}from"./index-6876ed87.js";const c=r("h1",null,"My Saved Books",-1),B={__name:"SavedBooksPage",setup(n){return(_,l)=>(o(),a(t,null,[c,s(e)],64))}};export{B as default};
