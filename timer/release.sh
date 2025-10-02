85aec12b9c205d830f5377e8cfa98ee97cd7ac3f
ckdev88 <ckdev88@outlook.com>
2025-10-02 10:23:05 +0200

    timer: update

--- timer/release.sh
+++
@@ -1,6 +0,0 @@
-rm -rf ~/Coding/ckdev88.github.io/timer/* && 
-cp -r ./* ~/Coding/ckdev88.github.io/timer/ &&
-cd ~/Coding/ckdev88.github.io/timer/ &&
-git add . &&
-git commit -m 'timer: update' && 
-git push
