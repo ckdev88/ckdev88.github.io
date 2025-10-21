2473fae35b37beada974041e467abbf807353bfb
ckdev88 <ckdev88@outlook.com>
2025-10-21 05:51:37 +0200

    timer: update

--- timer/script.js
+++ timer/script.js
@@ -1617,15 +1617,15 @@ new_timer_form.addEventListener('submit', (e) => {
 
 new_timer_quick.addEventListener('click', () => addQuickTimer())
 
-// register service worker for PWA
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/sw.js')
-      .then((registration) => {
-        console.log('SW registered: ', registration);
-      })
-      .catch((registrationError) => {
-        console.log('SW registration failed: ', registrationError);
-      });
-  });
-}
+// register service worker for PWA - does not work with static site like github.io
+// if ('serviceWorker' in navigator) {
+//   window.addEventListener('load', () => {
+//     navigator.serviceWorker.register('/sw.js')
+//       .then((registration) => {
+//         console.log('SW registered: ', registration);
+//       })
+//       .catch((registrationError) => {
+//         console.log('SW registration failed: ', registrationError);
+//       });
+//   });
+// }
