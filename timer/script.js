c507033c92568483f4120f0a301e7471302f0be3
ckdev88 <ckdev88@outlook.com>
2025-10-22 16:12:31 +0200

    timer: update

--- timer/script.js
+++ timer/script.js
@@ -252,6 +252,7 @@ const current_date = d.getElementById('current_date')
 function getRandomBackgroundAudio() {
     console.log('xzcvzxcvklasdjfghvnjklweasndfhgvjksd')
     let themood = moods[moods.findIndex((item) => settings.mood === item.mood)]
+    console.log('moods:',moods)
     console.log('themood:', themood)
     if (!themood) {
         themood = moods[0]
